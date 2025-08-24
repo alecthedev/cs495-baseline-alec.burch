85bea9a7a9479ad8195f835aa302a17f2bf6e5da
alecthedev <alecthedev@proton.me>
2025-08-24 13:23:28 -0400

    add my_slugify comment to utils.py

--- utils.py
+++ utils.py
@@ -2,4 +2,6 @@ import re
 
 
 def my_slugify(s: str) -> str:
+    # use regex to get substring containing only alphanumerics and dash
+    # from stripped, lowercased given string s with spaces replaces by dash
     return re.sub(r"[^-a-zA-Z0-9]", "", s.strip().lower().replace(" ", "-"))
